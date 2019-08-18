fe11d9ca9ee57182ae25ec12c6c7b7ee1df3790a
Jaap van der Kreeft <jaap@toxus.nl>
2019-08-18 11:55:18 +0200

    fix: version 1 remove

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const row2obj1 = require('./row-to-object-1');
+// const row2obj1 = require('./row-to-object-1');
 const row2obj2 = require('./row-to-object-2');
 
 module.exports = {
-  RowToObject1: row2obj1.RowToObject,
-  ErrorFieldNotFound : row2obj1.ErrorFieldNotFound,
-  charToIndex: row2obj1.charToIndex(),
+  // RowToObject1: row2obj1.RowToObject,
+  // ErrorFieldNotFound : row2obj1.ErrorFieldNotFound,
+  // charToIndex: row2obj1.charToIndex(),
 
-  RowToObject: row2obj2
+  RowToObject: row2obj2.RowToObject
 };
